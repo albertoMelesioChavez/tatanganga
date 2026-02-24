6fd3054569eef75f32e2f2ebc7b09f8c27355fa1
alberto.perse <alberto.perse@gmail.com>
2026-02-24 15:07:53 -0700

    Reset map to show entire world view

--- local/calendario/usermap.php
+++ local/calendario/usermap.php
@@ -153,7 +153,7 @@ $js = <<<JS
 (function() {
   var points = $pointsjson;
 
-  var map = L.map('usermap', { scrollWheelZoom: false }).setView([-15, -60], 3);
+  var map = L.map('usermap', { scrollWheelZoom: false }).setView([20, 0], 2);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 8,
     attribution: '&copy; OpenStreetMap'
