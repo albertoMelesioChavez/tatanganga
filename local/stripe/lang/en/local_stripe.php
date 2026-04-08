07875da93ebdfb89dc812dadea07b7dffd04a6d2
alberto.perse <alberto.perse@gmail.com>
2026-04-07 20:30:32 -0700

    Add language strings file for Stripe plugin to fix missing pluginname errors

---
+++ local/stripe/lang/en/local_stripe.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Stripe Subscriptions';
+$string['stripe'] = 'Stripe';
+$string['stripe:manage'] = 'Manage Stripe subscriptions';
+$string['publishablekey'] = 'Publishable Key';
+$string['publishablekey_desc'] = 'Your Stripe publishable key (starts with pk_)';
+$string['secretkey'] = 'Secret Key';
+$string['secretkey_desc'] = 'Your Stripe secret key (starts with sk_)';
+$string['webhooksecret'] = 'Webhook Signing Secret';
+$string['webhooksecret_desc'] = 'Your Stripe webhook signing secret (starts with whsec_)';
+$string['priceid'] = 'Price ID';
+$string['priceid_desc'] = 'Default Stripe Price ID for subscriptions (starts with price_)';
+$string['settings'] = 'Stripe Settings';
+$string['privacy:metadata'] = 'The Stripe plugin does not store any personal data.';
