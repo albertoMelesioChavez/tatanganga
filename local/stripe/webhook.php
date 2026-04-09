a91e22df72c2c2305f70df9c67b1932c87d878c9
alberto.perse <alberto.perse@gmail.com>
2026-04-09 14:51:31 -0700

    Enable webhook debug temporarily

--- local/stripe/webhook.php
+++ local/stripe/webhook.php
@@ -1,7 +1,7 @@
 <?php
 // Stripe webhook endpoint.
 
-define('NO_DEBUG_DISPLAY', true);
+define('NO_DEBUG_DISPLAY', false);
 define('CLI_SCRIPT', false);
 
 require_once(__DIR__ . '/../../config.php');
